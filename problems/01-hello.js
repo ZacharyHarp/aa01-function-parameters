7585cba337b6186932941839fc9a51dd2895b596
Andrea Fabro <andrea174633@gmail.com>
2024-06-08 13:38:04 -0700

    Problem 1

--- problems/01-hello.js
+++ problems/01-hello.js
@@ -6,12 +6,14 @@ console.log.
 */
 
 // Your code here 
-asdf
 // Note: If you console.log the return value of these function calls; you will get undefined. Remember a function that does not return anything evaluates to undefined.
+function hello(str) {
+    console.log("Hello, " + str);
+}
 
-// hello("cat"); // "Hello, cat"
-// hello("Helen"); // "Hello, Helen"
-// hello("Sting"); // "Hello, Sting"
+hello("cat"); // "Hello, cat"
+hello("Helen"); // "Hello, Helen"
+hello("Sting"); // "Hello, Sting"
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = hello;
